a46876de719df36908635ba499054773f1512ee2
Nicklas W <Nicklas2002@hotmail.com>
2023-09-04 00:21:23 +0200

    updated a little text

--- src/dk/cphbusiness/cphnw89/datastructures/threads/coffeeShop/Main.java
+++ src/dk/cphbusiness/cphnw89/datastructures/threads/coffeeShop/Main.java
@@ -8,22 +8,22 @@ public class Main {
     public static void main(String[] args) {
         CoffeeShop coffeeShop = new CoffeeShop();
 
-        ExecutorService customerExecutor = Executors.newFixedThreadPool(3);
+        ExecutorService customerExecutor = Executors.newFixedThreadPool(3); //Limit the amount of threads working on the task.
         ExecutorService baristaExecutor = Executors.newFixedThreadPool(2);
 
         for(int i = 0; i < 3; i++){
-            customerExecutor.execute(new Customer(coffeeShop, "Customer " + (i +1)));
+            customerExecutor.execute(new Customer(coffeeShop, "Customer " + (i +1))); //Execute the customer tasks
         }
 
         for(int i = 0; i < 2; i++){
-            baristaExecutor.execute(new Barista(coffeeShop, "Barista " + (i +1)));
+            baristaExecutor.execute(new Barista(coffeeShop, "Barista " + (i +1))); //Execute the barista tasks
         }
 
-        customerExecutor.shutdown();
+        customerExecutor.shutdown(); //Shuts down the threads and tasks. Waits for the tasks to be done before shutting down.
         baristaExecutor.shutdown();
 
         try{
-            customerExecutor.awaitTermination(10, TimeUnit.SECONDS);
+            customerExecutor.awaitTermination(10, TimeUnit.SECONDS); //Shuts down the tasks after shutdown is called or the timeout runs out.
             baristaExecutor.awaitTermination(10, TimeUnit.SECONDS);
         }catch(InterruptedException e){
             System.out.println(":(");
