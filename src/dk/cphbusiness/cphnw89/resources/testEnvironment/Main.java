e4a0f7f70fb5ee6fea3bce21d2499f270a6a6d76
Nicklas W <Nicklas2002@hotmail.com>
2023-09-18 09:08:34 +0200

    RessourceUpdate

---
+++ src/dk/cphbusiness/cphnw89/resources/testEnvironment/Main.java
@@ -0,0 +1,39 @@
+package dk.cphbusiness.cphnw89.resources.testEnvironment;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution("Hello"));
+
+        long number = 54321;
+        System.out.println(Arrays.toString(digitize(number)));
+    }
+
+    public static String solution(String str){
+        String output = "";
+        for(int i = str.length()-1; i > -1; i--){
+            output += str.charAt(i);
+        }
+        return output;
+    }
+
+    public static int[] digitize(long n) {
+        if(n > 0){
+            String number = Long.toString(n);
+            String[] numbers = number.split("");
+            String[] revNumbers = new String[numbers.length];
+            for(int i = 0; i < numbers.length; i++){
+                revNumbers[i] = numbers[numbers.length-1-i];
+            }
+            int[] output = new int[revNumbers.length];
+            for(int i = 0; i < revNumbers.length; i++){
+                output[i] = Integer.parseInt(revNumbers[i]);
+            }
+            return output;
+        }else {
+            return new int[]{0};
+        }
+    }
+
+}
